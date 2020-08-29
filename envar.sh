4db8c320862d91ccc0dd3201e23d9e3219900f38
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-08-29 16:30:05 +0530

    Delete envar.sh

--- envar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-if [ -v usersecret ] 
-then
-	echo variable is alredy assigned 
-else
-	echo variable is not assigned
-	export usersecret="dH34xJaa23"
- 	#printenv | grep usersecret
-fi
